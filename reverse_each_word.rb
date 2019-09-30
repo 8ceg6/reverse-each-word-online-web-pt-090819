707297257c979c4a96c220dc482f499ded4538fc
cedric e goddard <8ceg6@comcast.net>
2019-09-30 19:39:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
-split_sentence1 = sentence1.split(" ")
-reversed = []
-split_sentence1.each do |word|
+sentence1 = sentence1.split(" ")
+s1_reversed = []
+sentence1.each do |sentence1|
   reversed << word.reverse
 end
 reversed.join(" ")
